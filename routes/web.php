e09f47347d686bd83aad8942bb486a492022c64c
binhjkll <binh2022004@gmail.com>
2025-05-29 22:44:49 +0700

    login logout edit.user

--- routes/web.php
+++ routes/web.php
@@ -2,9 +2,13 @@
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Admin\PhongBanController;
+use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\Admin\RoleController;
+
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('auth.login');
 });
 
 Route::get('/dashboard', function () {
@@ -17,4 +21,60 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
+
+
+
+// Route::get('/', function () {
+//     return view('admin.dashboard.index');
+// });
+
+// Route::get('/dashboard', [DashboardController::class, 'index'])->name('home');
+
+// Admin Phòng Ban
+Route::get('/phongban', [PhongBanController::class, 'index']);
+Route::get('/phongban/create', [PhongBanController::class, 'create']);
+Route::post('/phongban/store', [PhongBanController::class, 'store']);
+Route::get('/phongban/show/{id}', [PhongBanController::class, 'show']);
+Route::get('/phongban/edit/{id}', [PhongBanController::class, 'edit']);
+Route::put('/phongban/update/{id}', [PhongBanController::class, 'update']);
+Route::delete('/phongban/delete/{id}', [PhongBanController::class, 'destroy']);
+
+// Admin Vai Trò
+Route::get('/vaitro/create', [RoleController::class, 'create'])->name('roles.create');
+Route::post('/vaitro/roles', [RoleController::class, 'store'])->name('roles.store');
+Route::get('/vaitro', [RoleController::class, 'index'])->name('roles.index');
+
+
+// Employee Routes
+Route::get('/employee', function () {
+    return view('layoutsEmploye.master');
+});
+
+Route::prefix('employee')->group(function () {
+
+    Route::get('/advance', function () {
+        return view('employe.advance');
+    });
+    Route::get('/attendance', function () {
+        return view('employe.attendance');
+    });
+    Route::get('/dashboard', function () {
+        return view('employe.dashboard');
+    });
+    Route::get('/leave', function () {
+        return view('employe.leave');
+    });
+    Route::get('/notification', function () {
+        return view('employe.notification');
+    });
+    Route::get('/profile', function () {
+        return view('employe.profile');
+    });
+    Route::get('/salary', function () {
+        return view('employe.salary');
+    });
+    Route::get('/task', function () {
+        return view('employe.task');
+    });
+});
