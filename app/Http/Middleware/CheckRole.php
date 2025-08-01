2034245fc52e21a478ca3555e0dbb2d84af6bb03
binhjkll <binh2022004@gmail.com>
2025-08-01 13:56:04 +0700

    sua loi chan nguio dung

--- app/Http/Middleware/CheckRole.php
+++ app/Http/Middleware/CheckRole.php
@@ -13,12 +13,25 @@ class CheckRole
     public function handle(Request $request, Closure $next, ...$roles)
     {
         $user = Auth::user();
+
+        // Nếu chưa login (phòng khi chưa chạy middleware 'auth'), thì bỏ qua
+        if (!$user) {
+            return redirect()->route('login');
+        }
+
+        // Nếu tài khoản bị vô hiệu hóa
+        if ($user->trang_thai == 0) {
+            Auth::logout(); // Đăng xuất
+            $request->session()->invalidate(); // Hủy session
+            $request->session()->regenerateToken(); // Reset CSRF token
+
+            return redirect()->route('login')->withErrors(['message' => 'Tài khoản của bạn đã bị vô hiệu hóa.']);
+        }
+
         $userRoles = optional($user->vaiTros)->pluck('ten')->toArray();
-        // dd(...$vaiTro);
 
         Log::info('Đang kiểm tra role cho route: ' . $request->path());
         Log::info('User Roles: ' . json_encode($userRoles));
-
 
         foreach ($roles as $role) {
             if (in_array($role, $userRoles)) {
@@ -26,7 +39,7 @@ class CheckRole
             }
         }
 
-        // Nếu quan hệ belongsTo
+        // Nếu quan hệ belongsTo (trong trường hợp khác)
         $userRole = optional($user->vaiTro)->ten;
 
         if (in_array($userRole, $roles)) {
